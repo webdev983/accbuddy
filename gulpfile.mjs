f0b80dd29a3b033ad1bddb99693d7146dcb47700
Sazedur Rahman <88509460+devsazedur@users.noreply.github.com>
2023-06-24 23:12:38 +0600

    Update gulpfile.mjs

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -12,3 +12,20 @@ gulp.task('build', async function() {
     await func2();
     console.log('done');
 });
+
+
+const gulp = require('gulp');
+const fileinclude = require('gulp-file-include');
+
+function buildPages() {
+  return gulp.src(['src/**/*.html'])
+    .pipe(fileinclude({
+      include: 'header.html',
+      base: './'
+    }))
+    .pipe(gulp.dest('docs'));
+}
+
+gulp.task('build', buildPages);
+
+gulp.task('default', ['build']);
