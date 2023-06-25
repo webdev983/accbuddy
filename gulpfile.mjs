a98d75f4af98870cd256ebd2c6b4fce70b4b3f1a
webdev983 <webdeveloper983@gmail.com>
2023-06-25 07:56:48 +0000

    .

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,32 +1,20 @@
-/*!!! plz use this syntax!!!!!!!!
-
- async function makeSoup() {
-  const pot = boilPot();
-  chopCarrots();
-  chopOnions();
-  await pot;
-  await boilPot();
-
-  addCarrots();
-  await letPotKeepBoiling(5);
-  addOnions();
-  await letPotKeepBoiling(10);
-  console.log(“your pot ready!”);
-}
-*/
-
-
 import gulp from 'gulp';
+import fileinclude from 'gulp-file-include';
 
-async function func1() {
+
+async function htmlPartialsInclude() {
+}
+  
+async function catalogDomsBuild() {
 }
 
-async function func2() {
-}
 
 gulp.task('build', async function() {
-    console.log('gulp build started');
-    await func1();
-    await func2();
-    console.log('done');
-});
+	console.log('gulp build started');
+	await htmlPartialsInclude();
+	await catalogDomsBuild();
+	console.log('done');
+  });
+
+  
+  
