66c7954cf51c664a5aa8f4f7a7eaa26605a6015d
zakir <zakirhossaib736@gmail.com>
2023-07-02 19:39:40 +0600

    updated partials and json works

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const postcss = require("gulp-postcss");
 const rename = require("gulp-rename");
 const minify = require('gulp-clean-css');
 var include = require('gulp-file-include');
-const beautify		= require('gulp-beautify');
+const beautify = require('gulp-beautify');
 const terser = require('gulp-terser');
 const imagewebp = require('gulp-webp');
 var sass = require('gulp-sass')(require('sass'));
@@ -23,16 +23,16 @@ var reload = browsersync.reload;
 gulp.task("browser-sync", function (done) {
   browsersync.init({
     server: {
-			baseDir: 'src/',
-		},
+      baseDir: 'docs/',
+    },
     // startPath: "src/index.html", 
     //    browser: 'chrome',
     host: 'localhost',
-       port: 5000,
+    port: 5000,
     open: true,
     tunnel: true
   });
-  gulp.watch(["./**/*.html"]).on("change", reload); 
+  gulp.watch(["./**/*.html"]).on("change", reload);
   done();
 });
 
@@ -65,7 +65,7 @@ gulp.task("css", () => {
     .pipe(rename({ suffix: ".min" }))
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(minify())
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("docs/css"))
     .pipe(browsersync.stream())
     .pipe(livereload())
 });
@@ -73,39 +73,42 @@ gulp.task("css", () => {
 // Webfonts task
 gulp.task("webfonts", () => {
   return (
-  gulp
-    .src("assests/scss/web/*.{ttf,woff,woff2,eot,svg}")
-    .pipe(gulp.dest('src/css/webfont'))
-    );
+    gulp
+      .src("assests/scss/web/*.{ttf,woff,woff2,eot,svg}")
+      .pipe(gulp.dest('docs/css/web'))
+  );
 });
 
 //optimize and move images
 gulp.task("webpImage", () => {
-  return( 
-  gulp
-.src('assests/image/**/*.{jpg,png}')
-.pipe(imagewebp())
-.pipe(gulp.dest('src/image'))
-);
+  return (
+    gulp
+      .src('assests/image/**/*.{jpg,png}')
+      .pipe(imagewebp())
+      .pipe(gulp.dest('docs/image'))
+  );
 });
 //svg images
 gulp.task("svgImage", () => {
-  return( 
-  gulp
-.src('assests/image/*.{svg,gif,pdf,webp}')
-.pipe(gulp.dest('src/image'))
-);
+  return (
+    gulp
+      .src('assests/image/*.{svg,gif,pdf,webp}')
+      .pipe(gulp.dest('docs/image'))
+  );
 });
 
 
 gulp.task("htmlInc", () => {
   return (
     gulp
-      .src(['html/*.html'])
+      .src([
+        'html/*.html',
+        'html/**/*.html'
+      ])
       .pipe(include())
-      .pipe(gulp.dest('src'))
+      .pipe(gulp.dest('docs'))
       .pipe(beautify.html({ indent_size: 1, indent_char: "	" }))
-      .pipe(browsersync.stream()) 
+      .pipe(browsersync.stream())
       .pipe(livereload())
   );
 });
@@ -114,7 +117,7 @@ gulp.task("htmlComponents", () => {
     gulp
       .src(['html/partials/**/_*.html'])
       .pipe(include())
-      .pipe(browsersync.stream()) 
+      .pipe(browsersync.stream())
       .pipe(livereload())
   );
 });
@@ -136,15 +139,15 @@ gulp.task("htmlComponents", () => {
 //   );
 // });
 
-gulp.task("default", gulp.series("css",  "webfonts", "webpImage","htmlInc","htmlComponents", "browser-sync", "svgImage", () => {
+gulp.task("default", gulp.series("css", "webfonts", "webpImage", "htmlInc", "htmlComponents", "browser-sync", "svgImage", () => {
   livereload.listen();
   gulp.watch(["assests/scss/**/*"], gulp.series("css"));
-//   gulp.watch(["assests/js/**/*"], gulp.series("js"));
+  //   gulp.watch(["assests/js/**/*"], gulp.series("js"));
   gulp.watch(["html/partials/**/_*.html"], gulp.series("htmlComponents"));
   gulp.watch(['html/*.html'], gulp.series("htmlInc"));
   gulp.watch(["assests/scss/web/*"], gulp.series("webfonts"));
-  gulp.watch('assests/image/*.{jpg,png}', gulp.series("webpImage")); 
-  gulp.watch('assests/image/*.{svg,gif,pdf}', gulp.series("svgImage")); 
+  gulp.watch('assests/image/*.{jpg,png}', gulp.series("webpImage"));
+  gulp.watch('assests/image/*.{svg,gif,pdf}', gulp.series("svgImage"));
 }));
 
 
