9aead2f9f8ae4506e927bc15a1a3c21a446740a1
zakir <zakirhossaib736@gmail.com>
2023-07-04 19:14:29 +0600

    updated package and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,37 +4,15 @@
 const autoprefixer = require("autoprefixer");
 const browsersync = require("browser-sync").create();
 const cssnano = require("cssnano");
-const concat = require('gulp-concat');
 const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const rename = require("gulp-rename");
-const minify = require('gulp-clean-css');
-var include = require('gulp-file-include');
-const beautify = require('gulp-beautify');
-const terser = require('gulp-terser');
-const imagewebp = require('gulp-webp');
-var sass = require('gulp-sass')(require('sass'));
-const livereload = require('gulp-livereload');
-var reload = browsersync.reload;
-
-
-
-// == Browser-sync task
-gulp.task("browser-sync", function (done) {
-  browsersync.init({
-    server: {
-      baseDir: 'docs/',
-    },
-    // startPath: "src/index.html", 
-    //    browser: 'chrome',
-    host: 'localhost',
-    port: 5000,
-    open: true,
-    tunnel: true
-  });
-  gulp.watch(["./**/*.html"]).on("change", reload);
-  done();
-});
+const minify = require("gulp-clean-css");
+const include = require("gulp-file-include");
+const beautify = require("gulp-beautify");
+const sass = require("gulp-sass")(require("sass"));
+const webp = require("gulp-webp");
+const livereload = require("gulp-livereload");
 
 // CSS task
 gulp.task("css", () => {
@@ -59,7 +37,7 @@ gulp.task("css", () => {
       "assests/scss/signIn-page.css",
       "assests/scss/signUp-page.css",
       "assests/scss/terms-service.css",
-      "assests/scss/style.scss"
+      "assests/scss/style.scss",
     ])
     .pipe(sass({ outputStyle: "expanded" }))
     .pipe(rename({ suffix: ".min" }))
@@ -67,90 +45,63 @@ gulp.task("css", () => {
     .pipe(minify())
     .pipe(gulp.dest("docs/css"))
     .pipe(browsersync.stream())
-    .pipe(livereload())
+    .pipe(livereload());
 });
 
 // Webfonts task
 gulp.task("webfonts", () => {
-  return (
-    gulp
-      .src("assests/scss/web/*.{ttf,woff,woff2,eot,svg}")
-      .pipe(gulp.dest('docs/css/web'))
-  );
+  return gulp.src("assests/scss/web/*.{ttf,woff,woff2,eot,svg}").pipe(gulp.dest("docs/css/web"));
 });
 
-//optimize and move images
-gulp.task("webpImage", () => {
-  return (
-    gulp
-      .src('assests/image/**/*.{jpg,png}')
-      .pipe(imagewebp())
-      .pipe(gulp.dest('docs/image'))
-  );
-});
-//svg images
-gulp.task("svgImage", () => {
-  return (
-    gulp
-      .src('assests/image/*.{svg,gif,pdf,webp}')
-      .pipe(gulp.dest('docs/image'))
-  );
+// Convert images to WebP
+gulp.task("webp", () => {
+  return gulp
+    .src("assests/image/**/*.{jpg,png}")
+    .pipe(webp())
+    .pipe(gulp.dest("docs/image"))
+    .pipe(browsersync.stream())
+    .pipe(livereload());
 });
 
-
-gulp.task("htmlInc", () => {
-  return (
-    gulp
-      .src([
-        'html/*.html',
-        'html/**/*.html'
-      ])
-      .pipe(include())
-      .pipe(gulp.dest('docs'))
-      .pipe(beautify.html({ indent_size: 1, indent_char: "	" }))
-      .pipe(browsersync.stream())
-      .pipe(livereload())
-  );
-});
-gulp.task("htmlComponents", () => {
-  return (
-    gulp
-      .src(['html/partials/**/_*.html'])
-      .pipe(include())
-      .pipe(browsersync.stream())
-      .pipe(livereload())
-  );
+// Copy other images (SVG, GIF) to docs folder
+gulp.task("otherImages", () => {
+  return gulp.src("assests/image/**/*.{svg,gif}").pipe(gulp.dest("docs/image"));
 });
 
-// Transpile, concatenate and minify scripts
-// gulp.task("js", () => {
-//   return (
-//     gulp
-//       .src([
-//         "node_modules/swiper/swiper-bundle.min.js",
-//         'assests/js/general.js'
-//       ])
-//       // folder only, filename is specified in webpack config
-//       .pipe(concat('app.js'))
-//       .pipe(terser())
-//       .pipe(gulp.dest("src/js"))
-//       .pipe(browsersync.stream())
-//       .pipe(livereload())
-//   );
-// });
+// HTML task
+gulp.task("html", () => {
+  return gulp
+    .src(["html/*.html", "html/**/*.html"])
+    .pipe(include())
+    .pipe(gulp.dest("docs"))
+    .pipe(beautify.html({ indent_size: 1, indent_char: "\t" }))
+    .pipe(browsersync.stream())
+    .pipe(livereload());
+});
 
-gulp.task("default", gulp.series("css", "webfonts", "webpImage", "htmlInc", "htmlComponents", "browser-sync", "svgImage", () => {
+// Watch files
+gulp.task("watch", () => {
   livereload.listen();
-  gulp.watch(["assests/scss/**/*"], gulp.series("css"));
-  //   gulp.watch(["assests/js/**/*"], gulp.series("js"));
-  gulp.watch(["html/partials/**/_*.html"], gulp.series("htmlComponents"));
-  gulp.watch(['html/*.html'], gulp.series("htmlInc"));
-  gulp.watch(["assests/scss/web/*"], gulp.series("webfonts"));
-  gulp.watch('assests/image/*.{jpg,png}', gulp.series("webpImage"));
-  gulp.watch('assests/image/*.{svg,gif,pdf}', gulp.series("svgImage"));
-}));
+  gulp.watch("assests/scss/**/*", gulp.series("css"));
+  gulp.watch("html/partials/**/_*.html", gulp.series("html"));
+  gulp.watch("html/*.html", gulp.series("html"));
+  gulp.watch("assests/scss/web/*", gulp.series("webfonts"));
+  gulp.watch("assests/image/**/*.{jpg,png}", gulp.series("webp"));
+  gulp.watch("assests/image/**/*.{svg,gif}", gulp.series("otherImages"));
+});
 
+// Serve task
+gulp.task("serve", () => {
+  browsersync.init({
+    server: {
+      baseDir: "docs/",
+    },
+    host: "localhost",
+    port: 5000,
+    open: true,
+    tunnel: true,
+  });
+});
 
-
-
-
+// Default task
+gulp.task("default", gulp.parallel("css", "webfonts", "webp", "otherImages", "html", "serve", "watch"));
