cee42ae8b8c2a07b0335578361d4299de3fa8760
webdev983 <webdeveloper983@gmail.com>
2023-07-11 11:39:01 -0400

    gtag

--- assests/js/gt.js
+++ assests/js/gt.js
@@ -7,17 +7,13 @@ async function loadScript() {
   });
 }
 
-function executeScript(event) {
+function executeScript() {
   loadScript().then(() => {
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-MTKNRB2JR5');
   });
-
-  // Удалить обработчик события после первого выполнения
-  document.removeEventListener('mousemove', executeScript);
 }
 
-// Добавить обработчик события, который будет вызван при движении мыши
-document.addEventListener('mousemove', executeScript);
+window.addEventListener('load', executeScript);
