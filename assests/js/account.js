fd2c3bcd3abf74ef1b9a98691cec94549216fef8
webdev983 <webdeveloper983@gmail.com>
2023-07-12 09:49:22 -0400

    1

--- assests/js/account.js
+++ assests/js/account.js
@@ -2,6 +2,6 @@
 function saveCart(PK) {
     const cart = { itemID: PK, quantity: "1" }
     localStorage.setItem('cart', JSON.stringify(cart))
-    window.location.href = "/shopping-cart.html";
+    window.location.href = "./shopping-cart.html";
 }
 
